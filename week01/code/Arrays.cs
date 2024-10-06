f7e457bca8cde265efd36e593a8df42d79f1a3e6
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-05 23:39:42 -0600

    Completed Dynamic Arrays Assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,19 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // I will make a loop that counts from 1 to length.
+        // Each time the loop runs, 'number' will be multiplied by the 'count' for that loop.
+        // Then I can add the outcome to the end of the array by using arrayName[count] = product of number and count.
+        // Alternatively, I could use arrayName.add(product of number and count)
+        // Then, the array will be returned.
+
+
+        double[] multiplesOf = new double[length];
+        foreach (double count in multiplesOf) {
+            multiplesOf[(int)count] = count * number;
+        }
+
+        return multiplesOf; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +41,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Get the 'size' of the array using .Count subtract by 1 for 0 indexing, use that number to grab the value at the end of the array.
+        // Store that value, remove it from the end of the array using .Remove(index) then add it to the beginning of the
+        // array using .Insert(value, 0). Then I can loop through this set of steps until I have rotated 'amount' of times.
+
+        int length = data.Count - 1;
+        int value = data[length];
+        for (int i = 1; i <= amount; i++) {
+            value = data[length];
+            data.Remove(length);
+            data.Insert(value, 0);
+        }
     }
 }
