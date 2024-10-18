a7bcce6e7e8996921095fb546889b606aad35b2a
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-17 23:59:07 -0600

    w03

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,11 +1,11 @@
 ﻿public class DuplicateCounter
 {
-    //Count how many duplicates are in a collection of data.
+  //Count how many duplicates are in a collection of data.
 
-    public static void Run()
-    {
-        int[] data = [
-            50, 9, 24, 100, 7, 75, 93, 24, 17, 16, 97, 6, 18, 81, 48, 37, 49, 33, 60, 3, 99, 32, 88, 29, 65, 20, 35, 33,
+  public static void Run()
+  {
+    int[] data = [
+        50, 9, 24, 100, 7, 75, 93, 24, 17, 16, 97, 6, 18, 81, 48, 37, 49, 33, 60, 3, 99, 32, 88, 29, 65, 20, 35, 33,
             15, 81, 31, 93, 17, 5, 5, 79, 12, 91, 18, 31, 12, 94, 39, 98, 10, 72, 20, 79, 100, 27, 46, 28, 50, 1, 7, 14,
             78, 100, 55, 26, 48, 33, 96, 77, 69, 8, 33, 36, 42, 98, 42, 32, 49, 65, 1, 82, 30, 74, 73, 89, 23, 76, 25,
             4, 76, 7, 72, 86, 71, 29, 18, 98, 84, 20, 24, 18, 11, 33, 39, 96, 1, 97, 65, 41, 62, 48, 59, 51, 17, 89, 6,
@@ -16,15 +16,23 @@
             6, 54, 10, 22, 16, 40, 67, 50, 58, 37, 35, 7, 44, 10, 31, 45, 93, 12, 55, 67, 48, 32, 43, 57, 58, 37, 76,
             85, 47, 80, 18, 32, 59, 98, 92, 53, 98, 29, 61, 82, 42, 78, 97, 23, 94, 38, 20, 73, 11, 99, 94, 92, 82, 82,
             65
-        ];
+    ];
 
-        Console.WriteLine($"Number of items in the collection: {data.Length}");
-        Console.WriteLine($"Number of duplicates : {CountDuplicates(data)}");
-    }
+    Console.WriteLine($"Number of items in the collection: {data.Length}");
+    Console.WriteLine($"Number of duplicates : {CountDuplicates(data)}");
+  }
 
-    private static int CountDuplicates(int[] data)
-    {
-        // Add code here.
+  private static int CountDuplicates(int[] data)
+  {
+        HashSet<int> seenNumbers = new HashSet<int>();
+        foreach (int number in data)
+        {
+            if (seenNumbers.Contains(number))
+            {
+                return 1;
+            }
+            seenNumbers.Add(number);
+        }
         return 0;
-    }
+  }
 }
