350244898fb2b9217ff542f7ca37a1535255401c
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-19 23:48:43 -0600

    w03

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -16,54 +16,49 @@
 /// </summary>
 public class Maze
 {
-    private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
-    private int _currX = 1;
-    private int _currY = 1;
+  private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
+  private int _currX = 1;
+  private int _currY = 1;
 
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
-    {
-        _mazeMap = mazeMap;
-    }
+  public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+  {
+    _mazeMap = mazeMap;
+  }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveLeft()
-    {
-        // FILL IN CODE
-    }
+  public void MoveLeft()
+  {
+    if (_mazeMap[(_currX, _currY)][0])
+      _currX--;
+    else
+      throw new InvalidOperationException("Can't go that way!");
+  }
 
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveRight()
-    {
-        // FILL IN CODE
-    }
+  public void MoveRight()
+  {
+    if (_mazeMap[(_currX, _currY)][1])
+      _currX++;
+    else
+      throw new InvalidOperationException("Can't go that way!");
+  }
 
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveUp()
-    {
-        // FILL IN CODE
-    }
+  public void MoveUp()
+  {
+    if (_mazeMap[(_currX, _currY)][2])
+      _currY--;
+    else
+      throw new InvalidOperationException("Can't go that way!");
+  }
 
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveDown()
-    {
-        // FILL IN CODE
-    }
+  public void MoveDown()
+  {
+    if (_mazeMap[(_currX, _currY)][3])
+      _currY++;
+    else
+      throw new InvalidOperationException("Can't go that way!");
+  }
 
-    public string GetStatus()
-    {
-        return $"Current location (x={_currX}, y={_currY})";
-    }
+  public string GetStatus()
+  {
+    return $"Current location (x={_currX}, y={_currY})";
+  }
 }
